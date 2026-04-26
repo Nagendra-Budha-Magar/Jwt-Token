4fb68212fa262a25a25c7367eab9f87f71c9006d
Ashish_Budha <nagendrabudha47@gmail.com>
2026-04-26 21:47:17 +0545

    Token-Creation

--- Application/Services/AuthService.cs
+++ Application/Services/AuthService.cs
@@ -1,5 +1,10 @@
 ﻿using JWT_Token.Models.Dtos;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace JWT_Token.Application.Services
 {
@@ -78,6 +83,7 @@ namespace JWT_Token.Application.Services
                 };
             }
 
+            // Checking password
             var result = await _signInManager.CheckPasswordSignInAsync(user, dto.Password, false);
             if (!result.Succeeded)
             {
@@ -88,8 +94,44 @@ namespace JWT_Token.Application.Services
                 };
             }
 
-            return await CreateToken(user);
+            var token = await CreateToken(user);
+
+            return new AppResponseDto
+            {
+                Success = true,
+                Token = token
+            };
         }
 
+        private async Task<string> CreateToken(IdentityUser user)
+        {
+            // This is the information about a subject.(Subject means user or admin)
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),  // What is subject id
+                new Claim(ClaimTypes.Name, user.UserName)       // What is subject name
+            };
+            // Adding role of a Subject
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            // Getting Key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value));
+            // Need for verifying if the token are correct using key + hash algorithm. Used for making 'Signature'
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            // Blueprint of token
+            var TokenDecriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = System.DateTime.Now.AddDays(1),
+                SigningCredentials = creds, // Tts a signuture
+                Issuer = _configuration.GetSection("Jwt:Issuer").Value,
+                Audience = _configuration.GetSection("Jwt:Audience").Value
+            };
+            // Actually creating and converting token into string
+            var TokenHandler = new JwtSecurityTokenHandler();
+            var Token = TokenHandler.CreateToken(TokenDecriptor);
+            return TokenHandler.WriteToken(Token);
+        }
     }
 }
